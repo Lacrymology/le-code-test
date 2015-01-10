313c5857aef88a3d75e22db6b9e0d347eb666ad5
Tomas Neme <lacrymology@gmail.com>
2015-01-10 15:42:55 +0700

    better admin lists

--- testsite/stream/admin.py
+++ testsite/stream/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from models import Stream
 
-admin.site.register(Stream)
+class StreamAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'photo', 'tweet')
+
+admin.site.register(Stream, StreamAdmin)
