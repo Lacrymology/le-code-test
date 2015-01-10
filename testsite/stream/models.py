24a153a23d66eb6982e1439bbde65c9ae895265a
Tomas Neme <lacrymology@gmail.com>
2015-01-10 14:33:05 +0700

    allow (and force) Stream objects to have photos or tweets

--- testsite/stream/models.py
+++ testsite/stream/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
 
 class Stream(models.Model):
     user = models.ForeignKey(User)
     created_at = models.DateTimeField()
+    photo = models.ForeignKey('items.PhotoItem', blank=True, null=True)
+    tweet = models.ForeignKey('items.TweetItem', blank=True, null=True)
+
+    def clean(self):
+        if not (self.photo or self.tweet):
+            raise ValidationError("Stream must have either a tweet or a photo")
